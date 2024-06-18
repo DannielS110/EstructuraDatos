baba399759284d059481d8be39b98f471e43e1e8
Daniel Sanchez <166523536+01010101010101011010010101101@users.noreply.github.com>
2024-06-17 22:38:44 -0500

    Implementacion Java
    clases en java

---
+++ JavaApplication1/src/javaapplication1/ejemplo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ejemplo {
+    
+    public void ejemplo1(Nodos n){
+        n.setValor(2);
+        System.out.print(n.getValor());
+    }
+    
+    public static void main(String[] args) {
+        
+             
+                
+         
+    }
+}
+
+
